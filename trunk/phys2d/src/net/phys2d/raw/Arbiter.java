4847692a9f631f21ba24860a7a1997e1ee6883fc
kevglass <kevglass@8c5acc88-331a-0410-b6fa-35ca3f5a3582>
2008-04-12 15:46:19 +0000

    Add stability on restitution check

--- trunk/phys2d/src/net/phys2d/raw/Arbiter.java
+++ trunk/phys2d/src/net/phys2d/raw/Arbiter.java
@@ -469,6 +469,14 @@ public strictfp class Arbiter {
 			b2.adjustVelocity(MathUtil.scale(impulse, b2.getInvMass(), temp));
 			b2.adjustAngularVelocity(b2.getInvI() * MathUtil.cross(r2, impulse));
 
+			// skip factors if the accumulated normal impulse is lower than tolerance
+			// this tolerance should be calculated on something else, but can't work out
+			// what
+			if ((c.accumulatedNormalImpulse / b1.getMass() < 5) &&
+			   (c.accumulatedNormalImpulse / b2.getMass() < 5)) {
+				c.restitution = 0;
+			}
+			
 			// Compute bias impulse
 			// NEW STUFF FOR SEPERATING BIAS
 			relativeVelocity.set(b2.getBiasedVelocity());
